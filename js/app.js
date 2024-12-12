27c9c66382224aa03cd2954e210db4e73d0b0bc6
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-12-12 09:37:35 +0600

    some ui change and donation button and history button active color change and show hide all section ok

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const donationBtn = document.getElementById('btn-donation');
+const historyBtn = document.getElementById('btn-history');
+const donationSections = document.getElementById('donationSection');
+const historySections = document.getElementById('history-section');
+
+
+function showDelete(whenClick){
+  donationSections.classList.add('hidden');
+  document.getElementById(whenClick).classList.remove('hidden');
+}
+
+donationBtn.addEventListener('click', function(){
+  donationBtn.classList.add('bg-green-500');
+  donationBtn.classList.remove('bg-gray-200');
+  historyBtn.classList.add('bg-gray-200');
+  historyBtn.classList.remove('bg-green-500');
+  showDelete('donationSection');
+});
+
+historyBtn.addEventListener('click', function(){
+  historyBtn.classList.add('bg-green-500');
+  historyBtn.classList.remove('bg-gray-200')
+  donationBtn.classList.add('bg-gray-200');
+  donationBtn.classList.remove('bg-green-500');
+  showDelete('history-section');
+});
