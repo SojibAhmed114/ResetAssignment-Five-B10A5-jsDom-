0238844ccb2f95fb248936678386f6c86b5a97f8
SojibAhmed114 <sojib.ahmed7620@gmail.com>
2024-12-12 18:50:31 +0600

    some donation account balance  desing change and section one javascript function all okey

--- js/app.js
+++ js/app.js
@@ -3,7 +3,20 @@ const historyBtn = document.getElementById('btn-history');
 const donationSections = document.getElementById('donationSection');
 const historySections = document.getElementById('history-section');
 
+// get input value and text innerText common function start here  
+function getInputValueUsingId(idName){
+  const inputValues = document.getElementById(idName).value;
+  const inputValueNumber = parseFloat(inputValues)
+  return inputValueNumber;
+}
+function getInnerTextUsingById (idNames){
+  const text = document.getElementById(idNames).innerText;
+  const textNumber = parseFloat(text);
+  return textNumber;
+}
+// get input value common function end here  
 
+// button navigation section start here 
 function showDelete(whenClick){
   donationSections.classList.add('hidden');
   historySections.classList.add('hidden')
@@ -25,3 +38,25 @@ historyBtn.addEventListener('click', function(){
   donationBtn.classList.remove('bg-green-500');
   showDelete('history-section');
 });
+// button navigation section end here 
+
+
+// donation section One worked start here 
+document.getElementById('btn-donation-One').addEventListener('click', function(){
+  const getValue = getInputValueUsingId('inputDonationOne');
+  const donationBalance = getInnerTextUsingById('donationBalanceONe');
+  const currentAccountBalance = getInnerTextUsingById('accountBalance');
+  if (isNaN(getValue) || getValue < 0) {
+    alert('invalid donation amount');
+    return;
+  }
+  else if (currentAccountBalance < getValue) {
+    alert('you have no insufficient balance');
+    return;
+  }
+  const decreaseAccountBalance = currentAccountBalance - getValue;
+  console.log(decreaseAccountBalance);
+  document.getElementById('accountBalance').innerText = decreaseAccountBalance;
+  const balance = donationBalance + getValue;
+  document.getElementById('donationBalanceONe').innerText = balance;
+})
